87fcf8dc7d396eed2fe1e0d4b95242a544efd2e0
Mathew Nitz <mbnitz@icloud.com>
2020-04-21 20:49:24 -0400

    Create serialization.cpp

---
+++ serialization.cpp
@@ -0,0 +1,5 @@
+//Boost serialization for CSCI 221 deadline project
+//Example on complex STL data structures
+//https://www.boost.org/doc/libs/1_71_0/libs/serialization/doc/index.html
+//Mathew Nitz, Spring 2020
+
