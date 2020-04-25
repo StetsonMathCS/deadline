fde9fea1a812c2d34b563db55bd8452651850548
Carson Sullivan <carson.sullivan35@gmail.com>
2020-04-25 00:00:00 -0400

    commit

---
+++ files.cpp
@@ -0,0 +1,55 @@
+#include <dirent.h>
+#include <sys/stat.h>
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+/* All our files will be stored in the /var/lib/deadline directory
+   of delenn so they can be accessed easily by the user
+
+   This code will list all the files present in the directory
+   to help keep list of what we have  
+*/
+
+void open(char *dir_name)
+{
+        DIR *dir; // pointer to directory
+        struct dirent *component; // directory contents
+        struct stat info;
+
+	int chmod(const char *dir_name, mode_t mode);
+
+        // open direcotry
+        dir = opendir(dir_name);
+
+        if(!dir)
+        {
+                cout << "\nDirectory Not Found\n" << endl;
+                return;
+        }
+
+        while((component = readdir(dir)) != NULL)
+        {
+                if(component->d_name[0] != '.')
+                {
+                        string path = string(component->d_name);
+                        cout << "File: " << path << "\t***in directory" << ": " << string(dir_name) << endl;
+                        stat(path.c_str(), &info); // get information from this path
+                        if(S_ISDIR(info.st_mode))
+                        {
+                                open((char*)path.c_str());
+				chmod(dir_name, S_IRWXO);
+                        }
+                }
+        }
+        closedir(dir);
+}
+
+int main()
+{
+        open((char*) "/var/lib/deadline");
+        return 0;
+}                                             
