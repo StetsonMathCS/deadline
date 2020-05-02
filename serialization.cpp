298848b432ce8cb6ebe1e496659130b8e521fc85
casefriendly <61064807+casefriendly@users.noreply.github.com>
2020-05-02 00:33:13 -0400

    Delete serialization.cpp

--- serialization.cpp
+++
@@ -1,6 +0,0 @@
-//Boost serialization for CSCI 221 deadline project
-//Example on complex STL data structures
-//https://www.boost.org/doc/libs/1_71_0/libs/serialization/doc/index.html
-//Mathew Nitz, Spring 2020
-
-//include boost/serialization/stl.hpp
